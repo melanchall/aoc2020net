2d36a224e0a83a180e01dde7656c6477cd2dd1f7
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-12-05 15:25:21 +0300

    Day 5 solved

---
+++ Aoc2020Net.Tests/Days/Day5Tests.cs
@@ -0,0 +1,16 @@
+﻿using Aoc2020Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2020Net.Tests.Days
+{
+    [DayDataPart1("FBFBBFFRLR", 357)]
+    [DayDataPart1("BFFFBBFRRR", 567)]
+    [DayDataPart1("FFFBBBFRRR", 119)]
+    [DayDataPart1("BBFFBBFRLL", 820)]
+    [DayDataPart1(801)]
+    [DayDataPart2(597)]
+    [TestFixture]
+    public sealed class Day5Tests : DayTests<Day5Tests>
+    {
+    }
+}
--- Aoc2020Net/Days/Day5.cs
+++ Aoc2020Net/Days/Day5.cs
@@ -1,17 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020Net.Days
 {
     internal sealed class Day5 : Day
     {
-        public override object SolvePart1()
-        {
-            throw new NotImplementedException();
-        }
+        private const int MaxRow = 127;
+        private const int MaxColumn = 7;
+
+        public override object SolvePart1() => GetSeatsIds().Max();
 
         public override object SolvePart2()
         {
-            throw new NotImplementedException();
+            var seatsIds = GetSeatsIds().ToArray();
+            return Enumerable
+                .Range(1, GetSeatId(MaxRow, MaxColumn))
+                .FirstOrDefault(id => !seatsIds.Contains(id) &&
+                                       seatsIds.Contains(id + 1) &&
+                                       seatsIds.Contains(id - 1));
+        }
+
+        private IEnumerable<int> GetSeatsIds() => InputData
+            .GetInputLines()
+            .Select(line => GetSeatId(GetSeatIdPart(line.Take(7), 'F', MaxRow),
+                                      GetSeatIdPart(line.Skip(7), 'L', MaxColumn)));
+
+        private int GetSeatId(int row, int column) => row * 8 + column;
+
+        private static int GetSeatIdPart(IEnumerable<char> directions, char lowerHalfSymbol, int rangeMax)
+        {
+            var rangeMin = 0;
+
+            foreach (var direction in directions)
+            {
+                if (direction == lowerHalfSymbol)
+                    rangeMax = rangeMin + (rangeMax - rangeMin) / 2;
+                else
+                    rangeMin = (rangeMax + rangeMin) / 2 + 1;
+            }
+
+            return rangeMin;
         }
     }
 }
