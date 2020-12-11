979a86f6201be88ee633aea235ceeb6a57742b10
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-12-11 09:36:00 +0300

    Day 11 solved

---
+++ Aoc2020Net.Tests/Days/Day11Tests.cs
@@ -0,0 +1,34 @@
+﻿using Aoc2020Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2020Net.Tests.Days
+{
+    [DayDataPart1(@"
+        L.LL.LL.LL
+        LLLLLLL.LL
+        L.L.L..L..
+        LLLL.LL.LL
+        L.LL.LL.LL
+        L.LLLLL.LL
+        ..L.L.....
+        LLLLLLLLLL
+        L.LLLLLL.L
+        L.LLLLL.LL", 37)]
+    [DayDataPart1(2183)]
+    [DayDataPart2(@"
+        L.LL.LL.LL
+        LLLLLLL.LL
+        L.L.L..L..
+        LLLL.LL.LL
+        L.LL.LL.LL
+        L.LLLLL.LL
+        ..L.L.....
+        LLLLLLLLLL
+        L.LLLLLL.L
+        L.LLLLL.LL", 26)]
+    [DayDataPart2(1990)]
+    [TestFixture]
+    public sealed class Day11Tests : DayTests<Day11Tests>
+    {
+    }
+}
--- Aoc2020Net/Days/Day11.cs
+++ Aoc2020Net/Days/Day11.cs
@@ -1,17 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aoc2020Net.Utilities;
 
 namespace Aoc2020Net.Days
 {
     internal sealed class Day11 : Day
     {
-        public override object SolvePart1()
+        private enum PlaceType
         {
-            throw new NotImplementedException();
+            [GridSymbol('.')] Floor = 0,
+            [GridSymbol('#')] OccupiedSeat,
+            [GridSymbol('L')] EmptySeat
         }
 
-        public override object SolvePart2()
+        public override object SolvePart1() => CountStabilizedOccupiedSeats(GetAdjacentPlaces, 4);
+
+        public override object SolvePart2() => CountStabilizedOccupiedSeats(GetVisiblePlaces, 5);
+
+        private int CountStabilizedOccupiedSeats(Func<PlaceType[,], int, int, int, int, IEnumerable<PlaceType>> seatsSelector, int minOccupiedSeatsNumberToMakeEmpty)
         {
-            throw new NotImplementedException();
+            var (grid, width, height) = InputData.GetInputGrid<PlaceType>();
+
+            while (true)
+            {
+                var newGrid = new PlaceType[width, height];
+
+                for (var x = 0; x < width; x++)
+                {
+                    for (var y = 0; y < height; y++)
+                    {
+                        newGrid[x, y] = grid[x, y] switch
+                        {
+                            PlaceType.EmptySeat => seatsSelector(grid, width, height, x, y).All(p => p != PlaceType.OccupiedSeat)
+                                ? PlaceType.OccupiedSeat
+                                : PlaceType.EmptySeat,
+                            PlaceType.OccupiedSeat => seatsSelector(grid, width, height, x, y).CountValue(PlaceType.OccupiedSeat) >= minOccupiedSeatsNumberToMakeEmpty
+                                ? PlaceType.EmptySeat
+                                : PlaceType.OccupiedSeat,
+                            _ => PlaceType.Floor
+                        };
+                    }
+                }
+
+                if (newGrid.OfType<PlaceType>().SequenceEqual(grid.OfType<PlaceType>()))
+                    return grid.OfType<PlaceType>().CountValue(PlaceType.OccupiedSeat);
+
+                grid = newGrid;
+            }
         }
+
+        private IEnumerable<PlaceType> GetAdjacentPlaces(PlaceType[,] grid, int width, int height, int pointX, int pointY)
+        {
+            for (var x = pointX - 1; x <= pointX + 1; x++)
+            {
+                for (var y = pointY - 1; y <= pointY + 1; y++)
+                {
+                    if ((x == pointX && y == pointY) || x < 0 || x >= width || y < 0 || y >= height)
+                        continue;
+
+                    yield return grid[x, y];
+                }
+            }
+        }
+
+        private IEnumerable<PlaceType> GetVisiblePlaces(PlaceType[,] grid, int width, int height, int x, int y)
+        {
+            yield return GetFirstSeat(Enumerable.Range(1, x).Select(i => grid[x - i, y]));
+            yield return GetFirstSeat(Enumerable.Range(x + 1, width - x - 1).Select(i => grid[i, y]));
+
+            yield return GetFirstSeat(Enumerable.Range(1, y).Select(i => grid[x, y - i]));
+            yield return GetFirstSeat(Enumerable.Range(y + 1, height - y - 1).Select(i => grid[x, i]));
+
+            yield return GetFirstSeat(Enumerable.Range(1, x)
+                .Zip(Enumerable.Range(1, y), (xx, yy) => new { X = x - xx, Y = y - yy })
+                .Where(p => p.X >= 0 && p.Y >= 0)
+                .Select(p => grid[p.X, p.Y]));
+
+            yield return GetFirstSeat(Enumerable.Range(x + 1, width - x - 1)
+                .Zip(Enumerable.Range(y + 1, height - y - 1), (xx, yy) => new { X = xx, Y = yy })
+                .Where(p => p.X < width && p.Y < height)
+                .Select(p => grid[p.X, p.Y]));
+
+            yield return GetFirstSeat(Enumerable.Range(1, x)
+                .Zip(Enumerable.Range(y + 1, height - y - 1), (xx, yy) => new { X = x - xx, Y = yy })
+                .Where(p => p.X >= 0 && p.Y < height)
+                .Select(p => grid[p.X, p.Y]));
+
+            yield return GetFirstSeat(Enumerable.Range(x + 1, width - x - 1)
+                .Zip(Enumerable.Range(1, y), (xx, yy) => new { X = xx, Y = y - yy })
+                .Where(p => p.X < width && p.Y >= 0)
+                .Select(p => grid[p.X, p.Y]));
+        }
+
+        private static PlaceType GetFirstSeat(IEnumerable<PlaceType> places) => places.FirstOrDefault(p => p != PlaceType.Floor);
     }
 }
