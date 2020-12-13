e47e916c3b2acb02c7d8ea1093bb0908ad2f30f0
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-12-13 13:13:51 +0300

    Day 13 solved

---
+++ Aoc2020Net.Tests/Days/Day13Tests.cs
@@ -0,0 +1,33 @@
+﻿using Aoc2020Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2020Net.Tests.Days
+{
+    [DayDataPart1(@"
+        939
+        7,13,x,x,59,x,31,19", 295)]
+    [DayDataPart1(3606)]
+    [DayDataPart2(@"
+        0
+        7,13,x,x,59,x,31,19", 1068781)]
+    [DayDataPart2(@"
+        0
+        17,x,13,19", 3417)]
+    [DayDataPart2(@"
+        0
+        67,7,59,61", 754018)]
+    [DayDataPart2(@"
+        0
+        67,x,7,59,61", 779210)]
+    [DayDataPart2(@"
+        0
+        67,7,x,59,61", 1261476)]
+    [DayDataPart2(@"
+        0
+        1789,37,47,1889", 1202161486)]
+    [DayDataPart2(379786358533423)]
+    [TestFixture]
+    public sealed class Day13Tests : DayTests<Day13Tests>
+    {
+    }
+}
--- Aoc2020Net/Days/Day13.cs
+++ Aoc2020Net/Days/Day13.cs
@@ -1,17 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020Net.Days
 {
     internal sealed class Day13 : Day
     {
+        private record Schedule(int StartTime, IEnumerable<int?> BusesIds);
+
         public override object SolvePart1()
         {
-            throw new NotImplementedException();
+            var (startTime, busesIds) = GetInputSchedule();
+
+            var nearestBus = busesIds
+                .Where(id => id != null)
+                .Select(id => new
+                {
+                    BusId = id,
+                    DepartureOffset = id * ((startTime / id) + 1) - startTime
+                })
+                .OrderBy(t => t.DepartureOffset)
+                .First();
+
+            return nearestBus.BusId * nearestBus.DepartureOffset;
         }
 
         public override object SolvePart2()
         {
-            throw new NotImplementedException();
+            var (_, busesIds) = GetInputSchedule();
+
+            var cms = busesIds
+                .Select((id, i) => new { Id = id, Offset = i })
+                .Where(id => id.Id != null)
+                .Select(x => new
+                {
+                    C = x.Id - x.Offset,
+                    M = x.Id.Value
+                })
+                .ToArray();
+            var ams = cms
+                .Select(cm => new
+                {
+                    A = cms.Where(xxx => xxx.M != cm.M).Aggregate(1L, (a, b) => a * b.M),
+                    M = cm.M
+                })
+                .ToArray();
+            var xs = ams
+                .Select(am =>
+                {
+                    GetGcdExtended(am.A, am.M, out var x, out _);
+                    return Enumerable.Range(0, int.MaxValue).Select(i => x + am.M * i).First(p => p > 0);
+                })
+                .ToArray();
+
+            return Enumerable.Range(0, cms.Length)
+                .Select(i => ams[i].A * xs[i] * cms[i].C)
+                .Sum() % cms.Aggregate(1L, (m, cm) => m * cm.M);
+        }
+
+        private Schedule GetInputSchedule()
+        {
+            var lines = InputData.GetInputLines();
+            var startTime = int.Parse(lines.First());
+            return new Schedule(
+                startTime,
+                lines
+                    .Last()
+                    .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => int.TryParse(s, out var id) ? (int?)id : null));
+        }
+
+        private void GetGcdExtended(long a, long b, out long x, out long y)
+        {
+            if (b == 0)
+            {
+                x = 1;
+                y = 0;
+                return;
+            }
+
+            GetGcdExtended(b, a % b, out x, out y);
+
+            var yTmp = y;
+            y = x - (a / b) * y;
+            x = yTmp;
         }
     }
 }
