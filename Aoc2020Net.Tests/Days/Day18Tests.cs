c8d60511bb3aae19e69c5d857955ccfec05525b7
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-12-18 12:30:19 +0300

    Day 18 solved

---
+++ Aoc2020Net.Tests/Days/Day18Tests.cs
@@ -0,0 +1,24 @@
+﻿using Aoc2020Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2020Net.Tests.Days
+{
+    [DayDataPart1("1 + 2 * 3 + 4 * 5 + 6", 71)]
+    [DayDataPart1("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+    [DayDataPart1("2 * 3 + (4 * 5)", 26)]
+    [DayDataPart1("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
+    [DayDataPart1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+    [DayDataPart1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+    [DayDataPart1(13976444272545)]
+    [DayDataPart2("1 + 2 * 3 + 4 * 5 + 6", 231)]
+    [DayDataPart2("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+    [DayDataPart2("2 * 3 + (4 * 5)", 46)]
+    [DayDataPart2("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+    [DayDataPart2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+    [DayDataPart2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+    [DayDataPart2(88500956630893)]
+    [TestFixture]
+    public sealed class Day18Tests : DayTests<Day18Tests>
+    {
+    }
+}
--- Aoc2020Net/Days/Day18.cs
+++ Aoc2020Net/Days/Day18.cs
@@ -1,17 +1,70 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Aoc2020Net.Utilities;
 
 namespace Aoc2020Net.Days
 {
     internal sealed class Day18 : Day
     {
-        public override object SolvePart1()
+        public override object SolvePart1() => GetEvaluatedExpressionsSum(EvaluateSimpleExpression);
+
+        public override object SolvePart2() => GetEvaluatedExpressionsSum(EvaluateSimpleExpressionWithAdditionFirst);
+
+        private long GetEvaluatedExpressionsSum(Func<string, long> evaluateSimpleExpression) =>
+            InputData.GetInputLines().Select(line => EvaluateExpression(line, evaluateSimpleExpression)).Sum();
+
+        private static long EvaluateExpression(string expression, Func<string, long> evaluateSimpleExpression)
         {
-            throw new NotImplementedException();
+            var subExpressionRegex = new Regex(@"\([^()]+?\)");
+            var subExpressionsCount = expression.CountValue('(');
+
+            for (var i = 0; i < subExpressionsCount; i++)
+            {
+                expression = subExpressionRegex.Replace(expression, m => evaluateSimpleExpression(m.Value.Trim('(', ')')).ToString());
+            }
+
+            return evaluateSimpleExpression(expression);
         }
 
-        public override object SolvePart2()
+        private static long EvaluateSimpleExpressionWithAdditionFirst(string expression)
         {
-            throw new NotImplementedException();
+            var expressionWithAdditionsInParentheses = Regex.Replace(expression, @"[\d\s+]+", m => $" ({m.Value.Trim()}) ");
+            return EvaluateExpression(expressionWithAdditionsInParentheses, EvaluateSimpleExpression);
+        }
+
+        private static long EvaluateSimpleExpression(string expression)
+        {
+            const char noOperation = ' ';
+            const string noNumber = "";
+
+            var result = 0L;
+
+            var operation = noOperation;
+            var numberString = noNumber;
+
+            foreach (var symbol in $"{expression}.".Where(c => c != ' '))
+            {
+                if (char.IsDigit(symbol))
+                    numberString += symbol;
+                else
+                {
+                    var number = long.Parse(numberString);
+                    result = operation == noOperation
+                        ? number
+                        : operation switch
+                        {
+                            '+' => result + number,
+                            '*' => result * number,
+                            _   => result
+                        };
+
+                    operation = symbol;
+                    numberString = noNumber;
+                }
+            }
+
+            return result;
         }
     }
 }
