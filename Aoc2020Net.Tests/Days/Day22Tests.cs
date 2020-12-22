971cfab1da8770a600956aff55dd9fa4ec1d5590
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-12-22 15:00:22 +0300

    Day 22 solved

---
+++ Aoc2020Net.Tests/Days/Day22Tests.cs
@@ -0,0 +1,40 @@
+﻿using Aoc2020Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2020Net.Tests.Days
+{
+    [DayDataPart1(@"
+        Player 1:
+        9
+        2
+        6
+        3
+        1
+
+        Player 2:
+        5
+        8
+        4
+        7
+        10", 306)]
+    [DayDataPart1(30197)]
+    [DayDataPart2(@"
+        Player 1:
+        9
+        2
+        6
+        3
+        1
+
+        Player 2:
+        5
+        8
+        4
+        7
+        10", 291)]
+    [DayDataPart2(34031)]
+    [TestFixture]
+    public sealed class Day22Tests : DayTests<Day22Tests>
+    {
+    }
+}
--- Aoc2020Net/Days/Day22.cs
+++ Aoc2020Net/Days/Day22.cs
@@ -1,17 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020Net.Days
 {
     internal sealed class Day22 : Day
     {
-        public override object SolvePart1()
+        public override object SolvePart1() => GetWinningScore(false);
+
+        public override object SolvePart2() => GetWinningScore(true);
+
+        private int GetWinningScore(bool playRecursive)
         {
-            throw new NotImplementedException();
+            var linesGroups = InputData.GetInputLinesGroups();
+            var firstPlayerDeck = new Queue<int>(linesGroups[0][1..].Select(int.Parse));
+            var secondPlayerDeck = new Queue<int>(linesGroups[1][1..].Select(int.Parse));
+
+            return (IsFirstPlayerWin(firstPlayerDeck, secondPlayerDeck, playRecursive) ? firstPlayerDeck : secondPlayerDeck)
+                .Reverse()
+                .Select((n, i) => n * (i + 1))
+                .Sum();
         }
 
-        public override object SolvePart2()
+        private static bool IsFirstPlayerWin(Queue<int> firstPlayerDeck, Queue<int> secondPlayerDeck, bool playRecursive)
         {
-            throw new NotImplementedException();
+            var firstPlayerTurns = new List<int[]>();
+            var secondPlayerTurns = new List<int[]>();
+
+            while (firstPlayerDeck.Any() && secondPlayerDeck.Any())
+            {
+                var x = firstPlayerDeck.Dequeue();
+                var y = secondPlayerDeck.Dequeue();
+
+                var firstPlayerWins = firstPlayerDeck.Count >= x && secondPlayerDeck.Count >= y && playRecursive
+                    ? IsFirstPlayerWin(new Queue<int>(firstPlayerDeck.Take(x)), new Queue<int>(secondPlayerDeck.Take(y)), playRecursive)
+                    : x > y;
+
+                if (firstPlayerWins)
+                {
+                    firstPlayerDeck.Enqueue(x);
+                    firstPlayerDeck.Enqueue(y);
+                }
+                else
+                {
+                    secondPlayerDeck.Enqueue(y);
+                    secondPlayerDeck.Enqueue(x);
+                }
+
+                if (firstPlayerTurns.Any(t => t.SequenceEqual(firstPlayerDeck)) &&
+                    secondPlayerTurns.Any(t => t.SequenceEqual(secondPlayerDeck)))
+                    return true;
+
+                firstPlayerTurns.Add(firstPlayerDeck.ToArray());
+                secondPlayerTurns.Add(secondPlayerDeck.ToArray());
+            }
+
+            return firstPlayerDeck.Any();
         }
     }
 }
