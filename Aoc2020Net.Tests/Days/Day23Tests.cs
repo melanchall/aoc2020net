c5206903ebc211b88707e38b9220ab4648fe62e4
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-12-23 14:51:20 +0300

    Day 23 solved

---
+++ Aoc2020Net.Tests/Days/Day23Tests.cs
@@ -0,0 +1,14 @@
+﻿using Aoc2020Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2020Net.Tests.Days
+{
+    [DayDataPart1("389125467", "67384529")]
+    [DayDataPart1("52937846")]
+    [DayDataPart2("389125467", 149245887792)]
+    [DayDataPart2(8456532414)]
+    [TestFixture]
+    public sealed class Day23Tests : DayTests<Day23Tests>
+    {
+    }
+}
--- Aoc2020Net/Days/Day23.cs
+++ Aoc2020Net/Days/Day23.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020Net.Days
 {
@@ -6,12 +7,93 @@ namespace Aoc2020Net.Days
     {
         public override object SolvePart1()
         {
-            throw new NotImplementedException();
+            var initialCups = GetInitialCups().ToArray();
+            var cupsNodes = Play(initialCups, 100);
+            
+            var cupNode = cupsNodes.Find(1);
+            return Enumerable
+                .Range(0, initialCups.Length - 1)
+                .Aggregate(string.Empty, (result, _) => result + (cupNode = GetNextCupNode(cupsNodes, cupNode)).Value);
         }
 
         public override object SolvePart2()
         {
-            throw new NotImplementedException();
+            var initialCups = GetInitialCups().ToArray();
+            var additionalCups = Enumerable.Range(initialCups.Max() + 1, 1_000_000 - initialCups.Length);
+            
+            var cupsNodes = Play(initialCups.Concat(additionalCups), 10_000_000);
+            
+            var cupNode = cupsNodes.Find(1);
+            var nextCupNode = GetNextCupNode(cupsNodes, cupNode);
+            return (long)nextCupNode.Value * GetNextCupNode(cupsNodes, nextCupNode).Value;
         }
+
+        private IEnumerable<int> GetInitialCups() => InputData.GetInputText().Select(c => int.Parse(c.ToString()));
+
+        private static LinkedList<int> Play(IEnumerable<int> initialCups, int iterations)
+        {
+            var cupsNodes = new LinkedList<int>();
+            var cupsToCupsNodes = new Dictionary<int, LinkedListNode<int>>();
+
+            foreach (var cup in initialCups)
+            {
+                cupsToCupsNodes[cup] = cupsNodes.AddLast(cup);
+            }
+
+            var currentCupNode = cupsNodes.First;
+
+            for (var i = 0; i < iterations; i++)
+            {
+                var cups = ExtractCupsAfter(cupsNodes, currentCupNode, cupsToCupsNodes);
+                var destinationCupNode = GetDestination(cupsNodes, currentCupNode.Value, cupsToCupsNodes);
+                InsertCupsAfter(cupsNodes, destinationCupNode, cups, cupsToCupsNodes);
+
+                currentCupNode = GetNextCupNode(cupsNodes, currentCupNode);
+            }
+
+            return cupsNodes;
+        }
+
+        private static void InsertCupsAfter(LinkedList<int> cupsNodes, LinkedListNode<int> afterCupNode, int[] cups, Dictionary<int, LinkedListNode<int>> cupsToCupsNodes)
+        {
+            for (var i = 0; i < cups.Length; i++)
+            {
+                cupsToCupsNodes[cups[i]] = afterCupNode = cupsNodes.AddAfter(afterCupNode, cups[i]);
+            }
+        }
+
+        private static LinkedListNode<int> GetDestination(LinkedList<int> cupsNodes, int currentCup, Dictionary<int, LinkedListNode<int>> cupsToCupsNodes)
+        {
+            currentCup--;
+
+            for (; currentCup > 0; currentCup--)
+            {
+                var node = cupsToCupsNodes[currentCup];
+                if (node != null)
+                    return node;
+            }
+
+            return cupsToCupsNodes[cupsNodes.Max()];
+        }
+
+        private static int[] ExtractCupsAfter(LinkedList<int> cupsNodes, LinkedListNode<int> afterCupNode, Dictionary<int, LinkedListNode<int>> cupsToCupsNodes)
+        {
+            var i = 0;
+            var result = new int[3];
+
+            for (var cupNode = GetNextCupNode(cupsNodes, afterCupNode); i < 3; i++)
+            {
+                var nextCupNode = GetNextCupNode(cupsNodes, cupNode);
+                cupsNodes.Remove(cupNode);
+                result[i] = cupNode.Value;
+                cupsToCupsNodes[cupNode.Value] = null;
+                cupNode = nextCupNode;
+            }
+
+            return result;
+        }
+
+        private static LinkedListNode<int> GetNextCupNode(LinkedList<int> numbers, LinkedListNode<int> current) =>
+            current.Next ?? numbers.First;
     }
 }
