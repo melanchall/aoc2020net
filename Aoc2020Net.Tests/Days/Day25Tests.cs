7a22c3654f41d5ed10f7271ed5c9ae3460864349
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2020-12-25 13:29:54 +0300

    Day 25 solved

---
+++ Aoc2020Net.Tests/Days/Day25Tests.cs
@@ -0,0 +1,15 @@
+﻿using Aoc2020Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2020Net.Tests.Days
+{
+    [DayDataPart1(@"
+        5764801
+        17807724", 14897079)]
+    [DayDataPart1(1890859)]
+    [DayDataPart2("MERRY CHRISTMAS")]
+    [TestFixture]
+    public sealed class Day25Tests : DayTests<Day25Tests>
+    {
+    }
+}
--- Aoc2020Net/Days/Day25.cs
+++ Aoc2020Net/Days/Day25.cs
@@ -1,17 +1,43 @@
-﻿using System;
-
-namespace Aoc2020Net.Days
+﻿namespace Aoc2020Net.Days
 {
     internal sealed class Day25 : Day
     {
         public override object SolvePart1()
         {
-            throw new NotImplementedException();
+            var publicKeys = InputData.GetInputInt64NumbersFromLines();
+            var cardLoopSize = FindLoopSizeByPublicKey(7, publicKeys[0]);
+            return TransformSubjectNumber(publicKeys[1], cardLoopSize);
         }
 
         public override object SolvePart2()
         {
-            throw new NotImplementedException();
+            return "MERRY CHRISTMAS";
         }
+
+        private long TransformSubjectNumber(long subjectNumber, int loopSize)
+        {
+            var value = 1L;
+
+            for (var i = 0; i < loopSize; i++)
+            {
+                TransformValue(ref value, subjectNumber);
+            }
+
+            return value;
+        }
+
+        private int FindLoopSizeByPublicKey(long subjectNumber, long publicKey)
+        {
+            var value = 1L;
+
+            for (var i = 0; ; i++)
+            {
+                TransformValue(ref value, subjectNumber);
+                if (value == publicKey)
+                    return i + 1;
+            }
+        }
+
+        private void TransformValue(ref long value, long subjectNumber) => value = (value * subjectNumber) % 20201227;
     }
 }
